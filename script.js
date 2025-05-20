4c52524c6d1b3a69240a655194c466a6d7aa0079
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-20 08:44:54 +0700

    chore: add variables for operands and operator

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
 const subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
 const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
 const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+
+let firstNumber;
+let secondNumber;
+let operator;
